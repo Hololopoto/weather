eb5b7b9391b445f5f1572abb1cc9851fe9ad9967
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-03-17 15:40:20 +0300

    Remove unnecessary test files and add axios dependency

---
+++ src/WeatherApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+import WeatClear from './Components/Images/clear.png'
+import WeatCloud from './Components/Images/cloud.png'
+import WeatDrizzle from './Components/Images/drizzle.png'
+import WeatHumidity from './Components/Images/humidity.png'
+import WeatRain from './Components/Images/rain.png'
+import Search from './Components/Images/search.png'
+import WeatSnow from './Components/Images/snow.png'
+import WeatWind from './Components/Images/wind.png'
+onst WeatherApp = () => {
+  return <div>WeatherApp</div>;
+};
+
+export default WeatherApp;
