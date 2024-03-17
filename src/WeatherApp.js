a590fd41350f2e25a2f9f955a24db80efe702514
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-03-17 15:42:11 +0300

    Refactor image imports in WeatherApp.js

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,14 +1,14 @@
 import React from "react";
 import axios from "axios";
-import WeatClear from './Components/Images/clear.png'
-import WeatCloud from './Components/Images/cloud.png'
-import WeatDrizzle from './Components/Images/drizzle.png'
-import WeatHumidity from './Components/Images/humidity.png'
-import WeatRain from './Components/Images/rain.png'
-import Search from './Components/Images/search.png'
-import WeatSnow from './Components/Images/snow.png'
-import WeatWind from './Components/Images/wind.png'
-onst WeatherApp = () => {
+import WeatClear from "./Components/Images/clear.png";
+import WeatCloud from "./Components/Images/cloud.png";
+import WeatDrizzle from "./Components/Images/drizzle.png";
+import WeatHumidity from "./Components/Images/humidity.png";
+import WeatRain from "./Components/Images/rain.png";
+import Search from "./Components/Images/search.png";
+import WeatSnow from "./Components/Images/snow.png";
+import WeatWind from "./Components/Images/wind.png";
+const WeatherApp = () => {
   return <div>WeatherApp</div>;
 };
 
