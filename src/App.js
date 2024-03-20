ec6f524b380dcb4386e0c5ec4b27d6aa4efa21bd
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-03-20 03:18:39 +0300

    Update weather API URL and display 3-day forecast

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
   const api_key = "d02c70fe49bf4a70b2f024a6aba89a19";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${api_key}`;
+  //   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${api_key}`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${api_key}`;
   const searchLocation = (event) => {
     if (event.key === "Enter") {
       if (location !== "") {
@@ -30,11 +31,11 @@ function App() {
   };
 
   return (
-    <div className=" w-[607px] h-[829px] m-auto mt-20 rounded-md  bg-gradient-to-r from-violet-500 to-fuchsia-500">
+    <div className=" w-[607px] h-[880px] m-auto mt-5 rounded-md  bg-gradient-to-r from-violet-500 to-fuchsia-500">
       <div className="flex items-center animate-pulse focus-within:animate-none  justify-center">
         <input
           type="text"
-          className=" city shadow-2xl placeholder:text-gray-500 focus:outline-none rounded-l-lg p-4 my-10 bg-white"
+          className=" city shadow-2xl placeholder:text-gray-500 focus:outline-none rounded-l-lg p-4 my-4 bg-white"
           placeholder="Name a City"
           value={location}
           onChange={(event) => setLocation(event.target.value)}
--- src/Weather.js
+++ src/Weather.js
@@ -16,41 +16,107 @@ const Weather = ({ weatherData }) => {
     Rain: Rain,
     Snow: Snow,
   };
-
+  // console.log(weatherData.city.country);
   // useEffect(() => {
   //   setMain(weatherData.weather[0].main);
   //   console.log("Mainnn", main);
   // }, [weatherData]);
   return (
     <div>
-      {weatherData.weather ? (
-        <div className="bg-white bg-opacity-20 w-[580px] h-[600px] rounded-md m-auto">
-          <div className="flex flex-col items-center align-top gap-4">
+      {weatherData.list ? (
+        <div className="bg-white bg-opacity-20 w-[580px] h-[500px] rounded-md m-auto">
+          <div className="flex flex-col items-center align-top">
             <img
-              src={`${weather[weatherData.weather[0].main]}`}
+              src={`${weather[weatherData.list[0].weather[0].main]}`}
               alt=""
               className=""
             />
             <h1 className="text-white text-8xl">
-              {weatherData.main.temp.toFixed(0)}&deg;C
+              {weatherData.list[0].main.temp.toFixed(0)}&deg;C
             </h1>
             <h1 className="text-white text-5xl">
-              {weatherData.name},{weatherData.sys.country}
+              {weatherData.city.name},{weatherData.city.country}
             </h1>
           </div>
-          <div className="element flex justify-between text-white m-20">
+          <div className="element flex justify-between text-white mx-20 my-10">
             <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
               <img src={Humidity} alt="" />
               <div className="data flex flex-col items-center ">
-                <h1 className="">{weatherData.main.humidity}%</h1>
+                <h1 className="">{weatherData.list[0].main.humidity}%</h1>
                 <h1 className="">Humidity</h1>
               </div>
             </div>
             <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
               <img src={Wind} alt="" />
               <div className="data flex flex-col items-center">
-                <h1 className="">{weatherData.wind.speed} Km/h</h1>
+                <h1 className="">{weatherData.list[0].wind.speed} Km/h</h1>
                 <h1 className="">Wind Speed</h1>
+              </div>
+            </div>
+          </div>
+          {/* 3 Günlük Tahmin */}
+          <div className="bg-white bg-opacity-20 w-[580px] h-[265px] rounded-md flex m-auto">
+            <div className="flex flex-col items-center justify-center mt-7 ">
+              <div className="flex flex-col items-center align-top">
+                <img
+                  src={`${weather[weatherData.list[0].weather[0].main]}`}
+                  alt=""
+                  className="h-[80px]"
+                />
+                <h1 className="text-white text-lg">
+                  {weatherData.list[0].main.temp.toFixed(0)}&deg;C
+                </h1>
+                <h1 className="text-white text-lg">
+                  {weatherData.city.name},{weatherData.city.country}
+                </h1>
+              </div>
+              <div className="element flex flex-col gap-3 text-white mx-10 mb-10">
+                <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
+                  <img src={Humidity} alt="" className="h-[30px]" />
+                  <div className="data flex flex-col text-sm items-center ">
+                    <h1 className="">{weatherData.list[0].main.humidity}%</h1>
+                    <h1 className="">Humidity</h1>
+                  </div>
+                </div>
+                <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
+                  <img src={Wind} alt="" className="h-[30px]" />
+                  <div className="data flex flex-col text-sm items-center">
+                    <h1 className="">{weatherData.list[0].wind.speed} Km/h</h1>
+                    <h1 className="">Wind Speed</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* 2.gün */}
+            <div className="flex flex-col items-center justify-center mt-7 ">
+              <div className="flex flex-col items-center align-top">
+                <img
+                  src={`${weather[weatherData.list[0].weather[0].main]}`}
+                  alt=""
+                  className="h-[80px]"
+                />
+                <h1 className="text-white text-lg">
+                  {weatherData.list[0].main.temp.toFixed(0)}&deg;C
+                </h1>
+                <h1 className="text-white text-lg">
+                  {weatherData.city.name},{weatherData.city.country}
+                </h1>
+              </div>
+              <div className="element flex flex-col gap-3 text-white mx-20 mb-10">
+                <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
+                  <img src={Humidity} alt="" className="h-[30px]" />
+                  <div className="data flex flex-col text-sm items-center ">
+                    <h1 className="">{weatherData.list[0].main.humidity}%</h1>
+                    <h1 className="">Humidity</h1>
+                  </div>
+                </div>
+                <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
+                  <img src={Wind} alt="" className="h-[30px]" />
+                  <div className="data flex flex-col text-sm items-center">
+                    <h1 className="">{weatherData.list[0].wind.speed} Km/h</h1>
+                    <h1 className="">Wind Speed</h1>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
