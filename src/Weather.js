e51a8f706ea8f272d6267e508693e37d161ba3a2
Alper Demir <105297896+Hololopoto@users.noreply.github.com>
2024-03-20 03:49:02 +0300

    Update styling and display of weather information

--- src/Weather.js
+++ src/Weather.js
@@ -24,21 +24,27 @@ const Weather = ({ weatherData }) => {
   return (
     <div>
       {weatherData.list ? (
-        <div className="bg-white bg-opacity-20 w-[580px] h-[500px] rounded-md m-auto">
+        <div className="bg-white bg-opacity-20 w-[580px] h-[460px] rounded-md m-auto">
           <div className="flex flex-col items-center align-top">
             <img
               src={`${weather[weatherData.list[0].weather[0].main]}`}
               alt=""
               className=""
             />
-            <h1 className="text-white text-8xl">
+            <h1 className="text-white text-6xl">
               {weatherData.list[0].main.temp.toFixed(0)}&deg;C
             </h1>
-            <h1 className="text-white text-5xl">
+            <h1 className="text-white text-4xl">
               {weatherData.city.name},{weatherData.city.country}
             </h1>
+            <h1 className="text-white text-xl">
+              {weatherData.list[0].dt_txt
+                .split(/(?:[-\s]+)/, 3)
+                .reverse()
+                .join(".")}
+            </h1>
           </div>
-          <div className="element flex justify-between text-white mx-20 my-10">
+          <div className="element flex justify-between text-white mx-20 mb-10 mt-5">
             <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
               <img src={Humidity} alt="" />
               <div className="data flex flex-col items-center ">
@@ -55,16 +61,23 @@ const Weather = ({ weatherData }) => {
             </div>
           </div>
           {/* 3 Günlük Tahmin */}
-          <div className="bg-white bg-opacity-20 w-[580px] h-[265px] rounded-md flex m-auto">
+          <div className="bg-white bg-opacity-20 w-[580px] h-[305px] rounded-md flex m-auto">
             <div className="flex flex-col items-center justify-center mt-7 ">
               <div className="flex flex-col items-center align-top">
+                <h1 className="text-white text-sm">
+                  {weatherData.list[8].dt_txt
+                    .split(/(?:[-\s]+)/, 3)
+                    .reverse()
+                    .join(".")}
+                </h1>
+
                 <img
-                  src={`${weather[weatherData.list[0].weather[0].main]}`}
+                  src={`${weather[weatherData.list[8].weather[0].main]}`}
                   alt=""
                   className="h-[80px]"
                 />
                 <h1 className="text-white text-lg">
-                  {weatherData.list[0].main.temp.toFixed(0)}&deg;C
+                  {weatherData.list[8].main.temp.toFixed(0)}&deg;C
                 </h1>
                 <h1 className="text-white text-lg">
                   {weatherData.city.name},{weatherData.city.country}
@@ -74,14 +87,14 @@ const Weather = ({ weatherData }) => {
                 <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
                   <img src={Humidity} alt="" className="h-[30px]" />
                   <div className="data flex flex-col text-sm items-center ">
-                    <h1 className="">{weatherData.list[0].main.humidity}%</h1>
+                    <h1 className="">{weatherData.list[8].main.humidity}%</h1>
                     <h1 className="">Humidity</h1>
                   </div>
                 </div>
                 <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
                   <img src={Wind} alt="" className="h-[30px]" />
                   <div className="data flex flex-col text-sm items-center">
-                    <h1 className="">{weatherData.list[0].wind.speed} Km/h</h1>
+                    <h1 className="">{weatherData.list[8].wind.speed} Km/h</h1>
                     <h1 className="">Wind Speed</h1>
                   </div>
                 </div>
@@ -90,13 +103,20 @@ const Weather = ({ weatherData }) => {
             {/* 2.gün */}
             <div className="flex flex-col items-center justify-center mt-7 ">
               <div className="flex flex-col items-center align-top">
+                <h1 className="text-white text-sm">
+                  {weatherData.list[16].dt_txt
+                    .split(/(?:[-\s]+)/, 3)
+                    .reverse()
+                    .join(".")}
+                </h1>
+
                 <img
-                  src={`${weather[weatherData.list[0].weather[0].main]}`}
+                  src={`${weather[weatherData.list[16].weather[0].main]}`}
                   alt=""
                   className="h-[80px]"
                 />
                 <h1 className="text-white text-lg">
-                  {weatherData.list[0].main.temp.toFixed(0)}&deg;C
+                  {weatherData.list[16].main.temp.toFixed(0)}&deg;C
                 </h1>
                 <h1 className="text-white text-lg">
                   {weatherData.city.name},{weatherData.city.country}
@@ -106,14 +126,14 @@ const Weather = ({ weatherData }) => {
                 <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
                   <img src={Humidity} alt="" className="h-[30px]" />
                   <div className="data flex flex-col text-sm items-center ">
-                    <h1 className="">{weatherData.list[0].main.humidity}%</h1>
+                    <h1 className="">{weatherData.list[16].main.humidity}%</h1>
                     <h1 className="">Humidity</h1>
                   </div>
                 </div>
                 <div className="datas flex  items-center gap-3 bg-white bg-opacity-30 rounded-3xl px-6 py-2">
                   <img src={Wind} alt="" className="h-[30px]" />
                   <div className="data flex flex-col text-sm items-center">
-                    <h1 className="">{weatherData.list[0].wind.speed} Km/h</h1>
+                    <h1 className="">{weatherData.list[16].wind.speed} Km/h</h1>
                     <h1 className="">Wind Speed</h1>
                   </div>
                 </div>
